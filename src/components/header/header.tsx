f7970ce2dbc6ca88c51960c52e6fc55c7f31afef
taichi221228 <taichi221228@icloud.com>
2023-01-09 09:59:36 +0900

    🔨 Create header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import Icon from "../icon/icon";
 export default component$(() => {
   return (
     <header class="py-10">
-      <div class="mx-auto flex w-11/12">
+      <div class="mx-auto flex w-11/12 max-w-7xl">
         <div>
           <a class="flex items-center gap-2 hover:text-rose-600" href="https://www.github.com/taichi221228" target="_blank">
             <Icon />
