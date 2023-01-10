e9112330ca6c9a01622984a6da6527addc9f53cf
taichi221228 <taichi221228@icloud.com>
2023-01-10 20:55:06 +0900

    💅🏼 Change design

--- src/components/works/works.tsx
+++ src/components/works/works.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import { Atropos } from "~/integrations/react/atropos";
 import { itemList } from "./mock";
 
 export default component$(() => {
@@ -8,12 +9,19 @@ export default component$(() => {
 
       <div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6">
         {itemList.map((item) => (
-          <div class="rounded-lg bg-slate-200 shadow-lg">
-            <div class="p-4">
-              <h3 class="mb-4 text-3xl">{item.title}</h3>
-              <p class="text-lg">{item.description}</p>
+          <Atropos key={item.id} client:visible>
+            <div class="group relative pb-[100%] hover:drop-shadow-xl">
+              <a href="#" class="absolute inset-0 h-full w-full overflow-hidden rounded-xl bg-slate-200 shadow-lg">
+                <img src="https://via.placeholder.com/150" alt="" class="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" />
+                <div class="relative p-4" data-atropos-offset="1">
+                  <h3 class="mb-4 text-3xl">{item.title}</h3>
+                  <p class="translate-y-4 text-lg opacity-0 transition-all duration-500 group-hover:translate-y-0 group-hover:opacity-100">
+                    {item.description}
+                  </p>
+                </div>
+              </a>
             </div>
-          </div>
+          </Atropos>
         ))}
       </div>
     </section>
