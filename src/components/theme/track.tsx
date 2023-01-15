f08a2eda3fca82466dec9bd8fcc17f76a86833bd
taichi221228 <taichi221228@icloud.com>
2023-01-15 13:05:17 +0900

    💅🏼 Tweak animation

--- src/components/theme/track.tsx
+++ src/components/theme/track.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default component$((props: Props) => {
   return (
-    <div class={`bg-primary-offset relative overflow-hidden transition-all ${props.isVisibleSignal.value ? "h-auto py-8 px-6" : "h-0"}`}>
+    <div class={`bg-primary-offset relative overflow-hidden px-6 py-8 transition-all ${props.isVisibleSignal.value || "-mt-48"}`}>
       <CloseButton onClick$={() => (props.isVisibleSignal.value = false)} class="absolute top-3 right-3" />
       <p class="mb-4 text-center">SELECT THEME</p>
       <div class="flex gap-4">
