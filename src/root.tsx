77b3769869786817d277f7fb9bae3f780d80c0df
taichi221228 <taichi221228@icloud.com>
2023-01-18 21:33:05 +0900

    ⚙️ Add font

--- src/root.tsx
+++ src/root.tsx
@@ -5,6 +5,8 @@ import { RouterHead } from "./components/router-head/router-head";
 import ThemeProvider from "./components/theme/provider";
 import globalStyles from "./global.css?inline";
 
+import "@fontsource/mochiy-pop-p-one";
+
 export default component$(() => {
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
@@ -23,7 +25,9 @@ export default component$(() => {
       </head>
       <body lang="en">
         <ThemeProvider>
-          <RouterOutlet />
+          <div class="relative z-10">
+            <RouterOutlet />
+          </div>
           <ServiceWorkerRegister />
         </ThemeProvider>
       </body>
