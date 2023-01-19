66fcd8740f6bd5c5ecabe93157063d4c8f1c8ef3
taichi221228 <taichi221228@icloud.com>
2023-01-19 21:39:50 +0900

    🧹 Format code

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -2,12 +2,10 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <>
-      <section>
-        <h2 class="text-tertiary text-lg">HELLO, MY NAME IS TAICHI</h2>
-        <h1 class="mt-2 mb-9 text-8xl font-bold">I make websites.</h1>
-        <p class="text-3xl">I'm a front-end developer for a website development company in Osaka.</p>
-      </section>
-    </>
+    <section>
+      <h2 class="text-tertiary text-lg">HELLO, MY NAME IS TAICHI</h2>
+      <h1 class="mt-2 mb-9 text-8xl font-bold">I make websites.</h1>
+      <p class="text-3xl">I'm a front-end developer for a website development company in Osaka.</p>
+    </section>
   );
 });
