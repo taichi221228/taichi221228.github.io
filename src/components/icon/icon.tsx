8bbe488d936ba34c02385369e2cd7b764ec0bb6b
taichi221228 <taichi221228@icloud.com>
2023-01-21 23:21:34 +0900

    ⚡️ Optimize image

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div>
+    <div class="relative h-12 w-12 rounded-full bg-[#C8E9FB]">
       <img src="/icon.jpg" alt="profile icon" class="rounded-full" width={48} height={48} />
     </div>
   );
